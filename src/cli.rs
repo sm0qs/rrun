dc365bd4c03b3334f09b032db1a53128f6852201
sm0qs <74928880+sm0qs@users.noreply.github.com>
2026-09-08 14:34:40 +0200

    feat: add CLI argument parsing with clap

---
+++ src/cli.rs
@@ -0,0 +1,17 @@
+use clap::{
+	Parser,
+	builder::{Styles, styling::AnsiColor},
+};
+
+fn get_styles() -> Styles {
+	Styles::styled()
+		.header(AnsiColor::Yellow.on_default())
+		.literal(AnsiColor::Cyan.on_default())
+}
+
+#[derive(Parser)]
+#[command(version, about, styles = get_styles())]
+pub struct Cli {
+	/// Script to run
+	pub script: String,
+}
