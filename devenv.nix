2da34528c23f79bbda7e4d874fe11ed7e4748c2c
sm0qs <74928880+sm0qs@users.noreply.github.com>
2026-09-09 02:25:43 +0200

    chore: update devenv lock and add cross-compilation targets

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  env.RUST_LOG = "trace";
+  env = {
+    RUST_LOG = "trace";
+    CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUSTFLAGS = "-L native=${pkgs.pkgsCross.mingwW64.windows.pthreads}/lib";
+  };
+
+  packages = [
+    pkgs.pkgsCross.mingwW64.stdenv.cc
+  ];
 
   languages.rust = {
     enable = true;
+    channel = "stable";
+    targets = [ "x86_64-pc-windows-gnu" ];
   };
 }
